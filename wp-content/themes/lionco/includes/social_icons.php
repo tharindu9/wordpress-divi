3c35c80b1eb0f024cc9382d6f562c26891d62501
tharindu <tharindu@dreamsquadgroup.com>
2019-12-22 19:16:31 +0530

    initial stage

--- wp-content/themes/lionco/includes/social_icons.php
+++ wp-content/themes/lionco/includes/social_icons.php
@@ -7,17 +7,17 @@
 		</a>
 	</li>
 <?php endif; ?>
-<?php if ( 'on' === et_get_option( 'divi_show_twitter_icon', 'on' ) ) : ?>
-	<li class="et-social-icon et-social-twitter">
+<?php if ( 'on' === et_get_option( 'divi_show_instagram_icon', 'on' ) ) : ?>
+	<li class="et-social-icon et-social-instagram">
 		<a href="<?php echo esc_url( et_get_option( 'divi_twitter_url', '#' ) ); ?>" class="icon">
-			<span><?php esc_html_e( 'Twitter', 'Divi' ); ?></span>
+			<span><?php esc_html_e( 'Instagram', 'Divi' ); ?></span>
 		</a>
 	</li>
 <?php endif; ?>
-<?php if ( 'on' === et_get_option( 'divi_show_google_icon', 'on' ) ) : ?>
-	<li class="et-social-icon et-social-google-plus">
+<?php if ( 'on' === et_get_option( 'divi_show_email_icon', 'on' ) ) : ?>
+	<li class="et-social-icon et-social-email-plus">
 		<a href="<?php echo esc_url( et_get_option( 'divi_google_url', '#' ) ); ?>" class="icon">
-			<span><?php esc_html_e( 'Google', 'Divi' ); ?></span>
+			<span><?php esc_html_e( 'Email', 'Divi' ); ?></span>
 		</a>
 	</li>
 <?php endif; ?>
